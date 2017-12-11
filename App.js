1f2e5e7edf7722a25e66ae71833c7e81434f1c87
globus40000 <globus40000@gmail.com>
2017-12-11 04:15:38 -0500

    [basics] Styles

--- App.js
+++ App.js
@@ -1,36 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-class Blink extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {showText: true};
-
-        // Toggle the state every second
-        setInterval(() => {
-            this.setState(prevState => {
-                return {showText: ! prevState.showText}
-            });
-        }, 1000);
-    }
-
-    render() {
-        let display = this.state.showText ? this.props.text : '';
-        return (
-            <Text>{display}</Text>
-        );
-    }
-}
-
-export default class BlinkApp extends Component {
+export default class LotsOfStyles extends Component {
     render() {
         return (
             <View>
-                <Blink text='I love to blink'/>
-                <Blink text='Yes blinking is so great'/>
-                <Blink text='Why did they ever take this out of HTML'/>
-                <Blink text='Look at me look at me look at me'/>
+                <Text style={styles.red}>just red</Text>
+                <Text style={styles.bigBlue}>just bigblue</Text>
+                <Text style={[styles.bigBlue, styles.red]}>bigblue, then red</Text>
+                <Text style={[styles.red, styles.bigBlue]}>red, then bigblue</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    bigBlue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    red: {
+        color: 'red'
+    }
+});
