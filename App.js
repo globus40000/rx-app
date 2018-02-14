f14775448ec39a62f4329c63ee19a89c2dea494d
globus40000 <globus40000@gmail.com>
2018-02-14 04:12:22 -0500

    [components] Image example 1

--- App.js
+++ App.js
@@ -1,84 +1,23 @@
 import React from 'react';
-import {View, FlatList, Text, TouchableOpacity} from 'react-native';
+import {View, Image} from 'react-native';
 
-export default class App extends React.Component {
-
+export default class DisplayAnImage extends React.Component {
     render() {
         return (
-            <View
-                style={{padding: 50}}>
-                <Text>Minimal Example:</Text>
-                <FlatList
-                    data={[{key: 'Item #1'}, {key: 'Item #2'}]}
-                    renderItem={({item}) => <Text>{item.key}</Text>}
-                    //ItemSeparatorComponent={() => <Text>----</Text>}
-                    //ListEmptyComponent={() => <Text>Empty list</Text>}
-                    //ListHeaderComponent={() => <Text>- header -</Text>}
-                    //ListFooterComponent={() => <Text>- footer -</Text>}
-                    //horizontal={true}
-                    //initialNumToRender={1}
-                    //inverted={true}
-                    //numColumns={3}
+            <View style={{padding: 50}}>
+                <Image
+                    style={{width: 66, height: 58}}
+                    source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
                 />
-                <Text style={{marginTop: 50}}>More complex, multi-select example:</Text>
-                <MultiSelectList
-                    data={[{id: 1, title: 'Item #1'}, {id: 2, title: 'Item #2'}]}/>
+                <Image
+                    style={{width: 50, height: 50}}
+                    source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
+                />
+                <Image
+                    style={{width: 100, height: 100}}
+                    source={require('./favicon.png')}
+                />
             </View>
-        )
-    }
-}
-
-class MyListItem extends React.PureComponent {
-    _onPress = () => {
-        this.props.onPressItem(this.props.id);
-    };
-
-    render() {
-        const textColor = this.props.selected ? "red" : "black";
-        return (
-            <TouchableOpacity onPress={this._onPress}>
-                <View>
-                    <Text style={{ color: textColor }}>
-                        {this.props.title}
-                    </Text>
-                </View>
-            </TouchableOpacity>
-        );
-    }
-}
-
-class MultiSelectList extends React.PureComponent {
-    state = {selected: (new Map(): Map<string, boolean>)};
-
-    _keyExtractor = (item, index) => item.id;
-
-    _onPressItem = (id: string) => {
-        // updater functions are preferred for transactional updates
-        this.setState((state) => {
-            // copy the map rather than modifying state.
-            const selected = new Map(state.selected);
-            selected.set(id, !selected.get(id)); // toggle
-            return {selected};
-        });
-    };
-
-    _renderItem = ({item}) => (
-        <MyListItem
-            id={item.id}
-            onPressItem={this._onPressItem}
-            selected={!!this.state.selected.get(item.id)}
-            title={item.title}
-        />
-    );
-
-    render() {
-        return (
-            <FlatList
-                data={this.props.data}
-                extraData={this.state}
-                keyExtractor={this._keyExtractor}
-                renderItem={this._renderItem}
-            />
         );
     }
 }
