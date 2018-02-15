c43221899b8246dbf9a008c948b141e7cb830e3b
globus40000 <globus40000@gmail.com>
2018-02-14 22:49:49 -0500

    [components] Modal

--- App.js
+++ App.js
@@ -1,22 +1,59 @@
-import React from 'react';
-import {View, Image, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button, Modal, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-    stretch: {
-        width: 100,
-        height: 200
+export default class MyComponent extends Component {
+    state = {
+        modalVisible: false,
+    };
+
+    openModal() {
+        this.setState({modalVisible:true});
     }
-});
 
-export default class DisplayAnImageWithStyle extends React.Component {
+    closeModal() {
+        this.setState({modalVisible:false});
+    }
+
     render() {
         return (
-            <View style={{padding: 50}}>
-                <Image
-                    style={styles.stretch}
-                    source={require('./favicon.png')}
+            <View style={styles.container}>
+                <Modal
+                    visible={this.state.modalVisible}
+                    animationType={'fade'}
+                    onRequestClose={() => this.closeModal()}
+                    //presentationStyle="pageSheet"
+                >
+                    <View style={styles.modalContainer}>
+                        <View style={styles.innerContainer}>
+                            <Text>This is content inside of modal component</Text>
+                            <Button
+                                onPress={() => this.closeModal()}
+                                title="Close modal"
+                            >
+                            </Button>
+                        </View>
+                    </View>
+                </Modal>
+                <Button
+                    onPress={() => this.openModal()}
+                    title="Open modal"
                 />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'grey',
+    },
+    innerContainer: {
+        alignItems: 'center',
+    },
+});
