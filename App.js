ed6e1db10c1a5ad994d237ba27c2134bb7ce314a
globus40000 <globus40000@gmail.com>
2018-02-14 23:00:59 -0500

    [components] Picker

--- App.js
+++ App.js
@@ -1,59 +1,24 @@
 import React, { Component } from 'react';
-import { Text, View, Button, Modal, StyleSheet } from 'react-native';
+import { View, Picker } from 'react-native';
 
 export default class MyComponent extends Component {
+
     state = {
-        modalVisible: false,
+        language: 'js',
     };
-
-    openModal() {
-        this.setState({modalVisible:true});
-    }
-
-    closeModal() {
-        this.setState({modalVisible:false});
-    }
 
     render() {
         return (
-            <View style={styles.container}>
-                <Modal
-                    visible={this.state.modalVisible}
-                    animationType={'fade'}
-                    onRequestClose={() => this.closeModal()}
-                    //presentationStyle="pageSheet"
+            <View style={{padding: 50}}>
+                <Picker
+                    selectedValue={this.state.language}
+                    onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}
+                    mode="dropdown"
                 >
-                    <View style={styles.modalContainer}>
-                        <View style={styles.innerContainer}>
-                            <Text>This is content inside of modal component</Text>
-                            <Button
-                                onPress={() => this.closeModal()}
-                                title="Close modal"
-                            >
-                            </Button>
-                        </View>
-                    </View>
-                </Modal>
-                <Button
-                    onPress={() => this.openModal()}
-                    title="Open modal"
-                />
+                    <Picker.Item label="Java" value="java" />
+                    <Picker.Item label="JavaScript" value="js" />
+                </Picker>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    modalContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: 'grey',
-    },
-    innerContainer: {
-        alignItems: 'center',
-    },
-});
