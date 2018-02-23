81f9bd2911f49751c1d26cbaebe43af8435c7b4f
globus40000 <globus40000@gmail.com>
2018-02-22 23:42:12 -0500

    [components] ProgressBarAndroid

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import { View, Picker } from 'react-native';
+import { View, ProgressBarAndroid } from 'react-native';
 
 export default class MyComponent extends Component {
 
-    state = {
-        language: 'js',
-    };
-
     render() {
         return (
-            <View style={{padding: 50}}>
-                <Picker
-                    selectedValue={this.state.language}
-                    onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}
-                    mode="dropdown"
-                >
-                    <Picker.Item label="Java" value="java" />
-                    <Picker.Item label="JavaScript" value="js" />
-                </Picker>
+            <View style={{padding: 100}}>
+                <ProgressBarAndroid
+                    styleAttr="Inverse"
+                    color="blue"/>
             </View>
         );
     }
